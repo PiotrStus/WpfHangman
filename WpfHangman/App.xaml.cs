5e17e12d48ba1a3f5ae0c3e7cfb5c1aeba1d6e5c
Piotr Stus <pm2024love@gmail.com>
2024-05-22 20:45:37 +0200

    Add project files.

---
+++ WpfHangman/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfHangman
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
